d8dce1f39ca87ac53789be48aac4d3bfbe957139
tovach <red5.dalv@gmail.com>
2022-08-19 07:14:02 +0300

    feat: add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.tsx",
   ],
   theme: {
+    colors: {
+      'bg1': '#16181e',
+      'bg2': '#21242d',
+      'primary': '#f9f9f9',
+      'secondary': '#00b9ae',
+    },
+    fontFamily: {
+      sans: ['Lato', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
